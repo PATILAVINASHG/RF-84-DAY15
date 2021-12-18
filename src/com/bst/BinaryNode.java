dfbc9ead88920ae376b0436c98518fa78375f548
avinashpatil <patilavinash223@gmail.com>
2021-12-18 13:18:52 +0530

    Add the BST files program file

---
+++ src/com/bst/BinaryNode.java
@@ -0,0 +1,29 @@
+package com.bst;
+
+public class BinaryNode<K extends Comparable<K>> {
+
+	K key;
+	BinaryNode<K> left;
+	BinaryNode<K> right;
+
+	public BinaryNode(K key) {
+		this.key = key;
+		this.left = null;
+		this.right = null;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder .append("MyBinaryNode{kay=" + key + "}");
+		
+		  if (left != null) { builder.insert(0, "MyBinaryNode{key=" + left.key 
+				  );} 
+		  if (right != null) { builder.append("-->MyBinaryNode{key=" +
+				  right.key + "}"); }
+		 
+		return builder.toString();
+				
+	}
+
+}
